e45e230560add94f5e7868b6e7b327b2bf576ac5
jakedolan443 <jakedolan443@protonmail.com>
2022-01-09 15:21:06 +0000

    Update chess.py

--- chess.py
+++ chess.py
@@ -4,6 +4,7 @@ from gevent import monkey
 import flask
 import base64
 import random
+import time
 from geventwebsocket.handler import WebSocketHandler
 import chess    
 from chess import Board, Move, STARTING_FEN
@@ -11,21 +12,10 @@ import sys
 from flask_cors import CORS, cross_origin
 import signal
 from contextlib import contextmanager
-
-
-class TimeoutException(Exception): pass
-
-@contextmanager
-def time_limit(seconds):
-    def signal_handler(signum, frame):
-        raise TimeoutException("Timed out!")
-    signal.signal(signal.SIGALRM, signal_handler)
-    signal.alarm(seconds)
-    try:
-        yield
-    finally:
-        signal.alarm(0)
+import chess.polyglot
         
+        
+MAX_DEPTH = 18
         
 app = flask.Flask(__name__)
 
@@ -79,6 +69,41 @@ square_table = {chess.PAWN:[
 -30,-40,-40,-50,-50,-40,-40,-30,
 -30,-40,-40,-50,-50,-40,-40,-30]}
 
+
+
+def order_moves(board, moves):
+    scores = {}
+    for move in moves:
+        board.push(move)
+        if board.turn:
+            scores[move] = -sys.maxsize
+        else:
+            scores[move] = sys.maxsize
+        for second_move in board.legal_moves:
+            board.push(second_move)
+            score = calculate_board_score(board)
+            if score > scores[move]:
+                scores[move] = score
+            board.pop()
+        board.pop()
+    new_moves = []
+    for i in range(len(scores)):
+        if board.turn:
+            min_item = max(scores, key=scores.get)
+        else:
+            min_item = min(scores, key=scores.get)
+        new_moves.append(min_item)
+        del scores[min_item]
+    return new_moves
+
+def generate_legal_moves(board):
+    moves = board.pseudo_legal_moves
+    new_moves = []
+    for move in moves:
+        if board.is_legal(move):
+            new_moves.append(move)
+    return new_moves
+
 def calculate_board_score(board):
     
     if board.is_checkmate():
@@ -86,8 +111,8 @@ def calculate_board_score(board):
             return -sys.maxsize
         else:
             return sys.maxsize
-    elif board.is_stalemate() or board.is_insufficient_material() or board.can_claim_threefold_repetition() or board.can_claim_fifty_moves():
-        return 0  # worth nothing, draw
+    elif board.is_stalemate():
+        return -50  # worth nothing, draw
     
     w = {"p":len(board.pieces(chess.PAWN, chess.WHITE))*100, "n":len(board.pieces(chess.KNIGHT, chess.WHITE))*320, "b":len(board.pieces(chess.BISHOP, chess.WHITE))*330, "r":len(board.pieces(chess.ROOK, chess.WHITE))*900, "q":len(board.pieces(chess.QUEEN, chess.WHITE))*900}
     b = {"p":len(board.pieces(chess.PAWN, chess.BLACK))*100, "n":len(board.pieces(chess.KNIGHT, chess.BLACK))*320, "b":len(board.pieces(chess.BISHOP, chess.BLACK))*330, "r":len(board.pieces(chess.ROOK, chess.BLACK))*900, "q":len(board.pieces(chess.QUEEN, chess.BLACK))*900}
@@ -102,24 +127,18 @@ def calculate_board_score(board):
             piece_dict[piece_type] -= (square_table[piece_type][chess.square_mirror(piece)])
 
     score = material + sum(piece_dict.values())
-    if board.turn:
-        if score < 20: ## endgame
-            pass # see readme
-    else:
-        if score > -20: ## endgame
-            pass # see readme
     
     if board.turn:
         return score
     else:
         return -score
     
-def prune(board, alpha_value, beta_value, depth=0, maxdepth=3):
+def prune(board, alpha_value, beta_value, depth=0, maxdepth=MAX_DEPTH):
     if depth >= maxdepth:
         return q_search(board, alpha_value=alpha_value, beta_value=beta_value)
     else:
         best_score = -sys.maxsize
-        for move in board.legal_moves:
+        for move in generate_legal_moves(board):
             board.push(move)   
             score = -prune(board, -beta_value, -alpha_value, depth=depth+1)
             board.pop()
@@ -142,7 +161,7 @@ def q_search(board, alpha_value=0, beta_value=0):
     if( alpha_value < stand_pat ):
         alpha_value = stand_pat
 
-    for move in board.legal_moves:
+    for move in generate_legal_moves(board):
         if board.is_capture(move):
             board.push(move)        
             score = -q_search(board, alpha_value=-beta_value, beta_value=-alpha_value) # negamax swap
@@ -155,8 +174,8 @@ def q_search(board, alpha_value=0, beta_value=0):
 
 
 
-def iterate_legal_moves(board, best_value=0, alpha_value=0, beta_value=0, depth=0, maxdepth=1):
-    for move in board.legal_moves:
+def iterate_legal_moves(board, best_value=0, alpha_value=0, beta_value=0, depth=0, maxdepth=MAX_DEPTH):
+    for move in order_moves(board, generate_legal_moves(board)):
         board.push(move)
         board_value = -prune(board, -beta_value, -alpha_value, depth=depth+1, maxdepth=maxdepth)
         if board_value > best_value:
@@ -168,8 +187,12 @@ def iterate_legal_moves(board, best_value=0, alpha_value=0, beta_value=0, depth=
     return best_idx_value
     
 
-def calculate_best_move(board, maxdepth=5):
-    best_move = iterate_legal_moves(board, best_value=-sys.maxsize, alpha_value=-sys.maxsize, beta_value=sys.maxsize, maxdepth=maxdepth)
+def calculate_best_move(board, maxdepth=MAX_DEPTH):
+    try:
+        time.sleep(0.33)
+        best_move = chess.polyglot.MemoryMappedReader("bookfish.bin").weighted_choice(board).move
+    except Exception as e:
+        best_move = iterate_legal_moves(board, best_value=-sys.maxsize, alpha_value=-sys.maxsize, beta_value=sys.maxsize, maxdepth=maxdepth)
     return best_move
     
 
@@ -197,5 +220,8 @@ def api_fen(fen):
         return "N/A", 404
 
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.run(host="0.0.0.0", port=8887)
+
 # do not run this in production !! use a proxy instead
